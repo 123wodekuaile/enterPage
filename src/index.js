75d6fb61c9f9c8a89f18ab29f90a5683444add75
qishi <cn322441@alibaba-inc.com>
2023-04-25 14:45:52 +0800

    update

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import '@/assets/css/base.css'
-import '@/assets/js/rem.js'
-const root = document.createElement('div')
-document.body.appendChild(root)
-new Vue({
-    render:h=>h(App)
-}).$mount(root)
