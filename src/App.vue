3434921d0afaad667821a8c5f5611726988da6d7
qishi <cn322441@alibaba-inc.com>
2023-04-25 14:29:23 +0800

    feat: 修改

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,6 @@
 <template>
     <div  class="content">
-        <div class="navBar">
-            <div class="block">
-                <i class="returnIcon returnStyle">&#xe655;</i>
-            </div>
-            <div style="flex: 1;">
-                <div class="okButton">
-                    <span>通过</span>
-                </div>
-            </div>
-            <div class="navRight">
-                <i class="returnIcon expandStyle">&#xe68d;</i>
-            </div>
-        </div>
-        <div class="main">
-            <div class="logoCard">
-                <p class="text1">校园防疫    人人有责</p>
-                <p class=text2>浙江工业大学宣</p>
-                <img class="logoStyle" src="./assets/images/logo3.png" alt="">
-            </div>
-            <div class="card">
-                <p>Everyone is responsible for edidemic prevention on campus</p>
-            </div>
-            <div class="textContent">
-                <p class="marginT10">
-                    <span class="bigText marginR10" style="color:#000;">张依</span>
-                    <span class="bigText marginR10" style="color:#3b4841">校园通行码</span>
-                    <span class="middleText" style="color:#3b4841">Campus Pass Code</span>
-                </p>
-                <p style="margin-top: 0.45rem;">
-                    <span style="color:#000;font-size:0.48rem">管理学院</span>
-                </p>
-                <p style="font-size:0.44rem;color:#000;margin-top:0.2rem">33250119960516022x</p>
-                <p style="margin-top:0.4rem;font-size:0.26rem;color:#a1a1a1;">生成时间 Generation time:</p>
-                <p style="margin-top:0.32rem;font-size:0.3rem;color:#000;">2020-06-08 00:00:00</p>
-                <p style="margin-top:0.32rem;font-size:0.26rem;color:#a1a1a1;">到期时间 Expire Time</p>
-                <p style="margin-top:0.32rem;font-size:0.3rem;color:#000;">2020-06-08 23:59:59</p>
-                <div style="margin-top:0.32rem;">
-                    <img class="imgStyle" src="./assets/images/ercode.png" alt="">
-                </div>
-                <p style="text-align: left;font-size: 0.26rem;line-height:0.5rem;margin-top:0.5rem;"> <span style="margin-right:0.5rem;"></span> 凭此码可在学校朝晖校区、屏峰校区、莫干山校区范围内配合有效证件通行，请主动出示，配合检查。</p>
-                <p style="text-align: left;font-size: 0.26rem;line-height:0.5rem"> <span style="margin-right:0.5rem;"></span> This code can be used to pass with valid documents in the Zhaohui campus.Pingfeng campus and Moganshan campus,plaease zhudong chushiyixi thanks for gad</p>
-            </div>
-        </div>
-
+        {text}
     </div>
 </template>
 <style lang="less">
